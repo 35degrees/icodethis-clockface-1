d45bee3e7740f4f6a5e5e96cf1e542af728ed895
Sam <samhooker@gmail.com>
2024-09-23 14:27:45 -0700

    inital commit

---
+++ script.js
@@ -0,0 +1,20 @@
+const hourHand = document.querySelector('.hour-hand')
+const secondHand = document.querySelector('.second-hand')
+const minuteHand = document.querySelector('.minute-hand')
+
+let degrees = 0
+function showTime() {
+	const time = new Date()
+	const hour = time.getHours()
+	const minute = time.getMinutes()
+	const second = time.getSeconds()
+
+	console.log(`The time is ${hour}:${minute}:${second}`)
+
+	secondHand.style.transform = `rotate(${(second / 60) * 360 - 90}deg)`
+	console.log({ second })
+	minuteHand.style.transform = `rotate(${(minute / 60) * 360 - 90}deg)`
+	hourHand.style.transform = `rotate(${(hour / 12) * 360 - 90}deg)`
+}
+
+setInterval(showTime, 1000)
