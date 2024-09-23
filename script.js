fdee1dc8b26a5c8cc08fde31972db4748fb827a0
Sam <samhooker@gmail.com>
2024-09-23 14:32:43 -0700

    update

--- script.js
+++ script.js
@@ -4,18 +4,19 @@ const minuteHand = document.querySelector('.minute-hand')
 
 let degrees = 0
 function showTime() {
-	const time = new Date()
-	const hour = time.getHours()
-	const minute = time.getMinutes()
-	const second = time.getSeconds()
+	setInterval(() => {
+		const time = new Date()
+		const hour = time.getHours()
+		const minute = time.getMinutes()
+		const second = time.getSeconds()
 
-	console.log(`The time is ${hour}:${minute}:${second}`)
+		console.log(`The time is ${hour}:${minute}:${second}`)
 
-	secondHand.style.transform = `rotate(${(second / 60) * 360 - 90}deg)`
-	console.log({ second })
-	minuteHand.style.transform = `rotate(${(minute / 60) * 360 - 90}deg)`
-	hourHand.style.transform = `rotate(${(hour / 12) * 360 - 90}deg)`
-	setInterval(showTime, 1000)
+		secondHand.style.transform = `rotate(${(second / 60) * 360 - 90}deg)`
+		console.log({ second })
+		minuteHand.style.transform = `rotate(${(minute / 60) * 360 - 90}deg)`
+		hourHand.style.transform = `rotate(${(hour / 12) * 360 - 90}deg)`
+	}, 1000)
 }
 
 showTime()
